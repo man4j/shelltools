d992880302c145c743e37fecb6e99ce6dab1c991
root <root@dc1.1>
2017-07-23 11:37:49 +0200

    fix iptable save

--- prepare_docker_host.sh
+++ prepare_docker_host.sh
@@ -7,7 +7,7 @@ fi
 engine_label=$1
 
 apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && \
-apt-get install -y mc ntp software-properties-common apt-transport-https curl netfilter-persistent && \
+apt-get install -y mc ntp software-properties-common apt-transport-https curl iptables-persistent netfilter-persistent && \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
 apt-key fingerprint 0EBFCD88 && \
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
