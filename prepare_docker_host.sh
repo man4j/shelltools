b27963dd95305cd015288d7bd1c4bf22f0b151ce
root <man4j@ya.ru>
2017-09-11 16:17:56 +0200

    fixes

--- prepare_docker_host.sh
+++ prepare_docker_host.sh
@@ -6,30 +6,6 @@ fi
 
 engine_label=$1
 
-apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" update && \
-apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade -y && \
-apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade -y && \
-echo debconf iptables-persistent/autosave_done select true | debconf-set-selections && \
-echo debconf iptables-persistent/autosave_done seen true | debconf-set-selections && \
-apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -y mc ntp software-properties-common apt-transport-https curl iptables-persistent netfilter-persistent && \
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
-apt-key fingerprint 0EBFCD88 && \
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
-apt-get update && \
-apt-get install -y docker-ce && \
-netfilter-persistent flush && \
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT && \
-iptables -A INPUT -p tcp --dport 2376 -j ACCEPT && \
-iptables -A INPUT -p tcp --dport 2377 -j ACCEPT && \
-iptables -A INPUT -p tcp --dport 7946 -j ACCEPT && \
-iptables -A INPUT -p udp --dport 7946 -j ACCEPT && \
-iptables -A INPUT -p udp --dport 4789 -j ACCEPT && \
-netfilter-persistent save && \
-\
-echo '{"labels": ["dc='${engine_label}'"]}' >> /etc/docker/daemon.json && \
-service docker restart && \
-\
-echo -e "\nClientAliveInterval 60\nTCPKeepAlive yes\nClientAliveCountMax 180\n" >> /etc/ssh/sshd_config && \
 apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" update
 apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade -y
 apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade -y
