4d04b14e9175e9255449b0068d7e312d620aed9e
root <root@server1>
2018-01-28 14:38:50 +0000

    add centos script

---
+++ install_docker_centos.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then..
+  echo -e "\nERROR: Param dc label not specified\n"
+fi
+
+dc_label=$1
+
+yum -y install yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum -y install docker-ce
+systemctl start docker && systemctl enable docker
+echo '{"labels": ["dc='${dc_label}'"]}' >> /etc/docker/daemon.json
+systemctl restart docker
+
+firewall-cmd --add-port=2376/tcp --permanent
+firewall-cmd --add-port=2377/tcp --permanent
+firewall-cmd --add-port=7946/tcp --permanent
+firewall-cmd --add-port=7946/udp --permanent
+firewall-cmd --add-port=4789/udp --permanent
+firewall-cmd --reload
+
+docker plugin install --grant-all-permissions --alias vsphere vmware/vsphere-storage-for-docker:latest
+docker plugin install --alias weave weaveworks/net-plugin:latest_release
+
+echo -e "\nClientAliveInterval 60\nTCPKeepAlive yes\nClientAliveCountMax 180\n" >> /etc/ssh/sshd_config
+service sshd restart
--- install_docker_ubuntu.sh
+++ install_docker_ubuntu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then  
-  echo -e "\nERROR: Param engine label not specified\n"
+  echo -e "\nERROR: Param dc label not specified\n"
 fi
 
-engine_label=$1
+dc_label=$1
 
 apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" update
 apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade -y
@@ -21,8 +21,8 @@ apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce
-echo '{"labels": ["dc='${engine_label}'"]}' >> /etc/docker/daemon.json
-service docker restart 
+echo '{"labels": ["dc='${dc_label}'"]}' >> /etc/docker/daemon.json
+service docker restart
 
 netfilter-persistent flush
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
@@ -33,10 +33,8 @@ iptables -A INPUT -p udp --dport 7946 -j ACCEPT
 iptables -A INPUT -p udp --dport 4789 -j ACCEPT
 netfilter-persistent save
 
+docker plugin install --grant-all-permissions --alias vsphere vmware/vsphere-storage-for-docker:latest
+docker plugin install --alias weave weaveworks/net-plugin:latest_release
+
 echo -e "\nClientAliveInterval 60\nTCPKeepAlive yes\nClientAliveCountMax 180\n" >> /etc/ssh/sshd_config
 service sshd restart
-
-sysctl -w vm.max_map_count=262144
-echo "vm.max_map_count=262144" > /etc/sysctl.d/60-max_map_count.conf
-
-
