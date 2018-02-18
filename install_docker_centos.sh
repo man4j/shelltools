5ef37ddadb27a7904acabdcf310f96589daef46a
Vladimir Petrukhin <man4j@ya.ru>
2018-02-18 13:08:59 +0300

    Update install_docker_centos.sh

--- install_docker_centos.sh
+++ install_docker_centos.sh
@@ -6,7 +6,7 @@ fi
 
 dc_label=$1
 
-yum -y install yum-utils device-mapper-persistent-data lvm2
+yum -y install yum-utils device-mapper-persistent-data lvm2 ntp
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum -y install docker-ce
 systemctl start docker && systemctl enable docker
@@ -25,3 +25,6 @@ docker plugin install --alias weave weaveworks/net-plugin:latest_release
 
 echo -e "\nClientAliveInterval 60\nTCPKeepAlive yes\nClientAliveCountMax 180\n" >> /etc/ssh/sshd_config
 service sshd restart
+
+systemctl enable ntpd
+systemctl start ntpd
