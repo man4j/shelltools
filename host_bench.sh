30be19ddbe07344b6334bd968168ba07aae5d775
Vladimir Petrukhin <man4j@ya.ru>
2020-01-16 12:10:11 +0300

    Update host_bench.sh

--- host_bench.sh
+++ host_bench.sh
@@ -2,14 +2,19 @@
 
 mkdir /home/bench/
 cd /home/bench/
+
 echo "Testing write speed..."
 dd if=/dev/zero of=diskbench bs=1M count=1024 conv=fdatasync
 echo 3 | tee /proc/sys/vm/drop_caches
+
 echo "Testing reading without cache..."
 dd if=diskbench of=/dev/null bs=1M count=1024
+
 echo "Testing reading with cache..."
 dd if=diskbench of=/dev/null bs=1M count=1024
 rm -f diskbench
+
+#grep -E '^model name|^cpu MHz' /proc/cpuinfo
 echo "Testing cpu..."
 dd if=/dev/zero bs=1M count=1024 | md5sum
 echo "CPU info:"
